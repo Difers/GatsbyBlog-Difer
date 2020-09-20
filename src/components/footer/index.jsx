22b3c64a2d6dcc2595cf721719c3f392bc98d957
root <root@DESKTOP-P0L2DAR.localdomain>
2020-09-21 00:33:44 +0800

    upyun logo add

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -8,5 +8,10 @@ export const Footer = () => (
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
+    <br />
+    <div >
+      <a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referra"><img class="testIMG" src="https://gitee.com/difers/picgo/raw/master/img/upyun_1.png" align="absmiddle" width="59px" height="30px" /></a>
+      <span class="testTXT">提供加速服务</span>
+    </div>
   </footer>
 )
