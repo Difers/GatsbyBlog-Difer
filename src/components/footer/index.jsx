bf3bbcaadeba94ed8a60bedb8982b1cda69af1f1
root <root@DESKTOP-P0L2DAR.localdomain>
2020-09-21 10:58:55 +0800

    upyun to cent

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,15 +3,16 @@ import React from 'react'
 import './index.scss'
 
 export const Footer = () => (
-  <footer className="footer">
+  <footer className="footer" >
     ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
     <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
       Gatsby-starter-bee
     </a>
     <br />
-    <div >
-      <a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referra"><img class="testIMG" src="https://gitee.com/difers/picgo/raw/master/img/upyun_1.png" align="absmiddle" width="59px" height="30px" /></a>
-      <span class="testTXT">提供加速服务</span>
-    </div>
+    <a href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referra">
+      <img align="absmiddle" src="https://gitee.com/difers/picgo/raw/master/img/upyun_1.png" width="15%" height="15%" />
+      <span> 提供加速服务</span>
+
+    </a>
   </footer>
 )
