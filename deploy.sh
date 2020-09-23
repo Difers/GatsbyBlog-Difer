ae7363b7ac062078261b4cdd0299c8c2cc158aea
Difer <c7070655110@gmail.com>
2020-09-23 19:40:40 +0800

    Add files via upload
    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+# 确保脚本抛出遇到的错误
+set -e
+yarn develop && yarn run build # 生成静态文件
+cd public # 进入生成的文件夹
+
+# deploy to github
+echo 'difer.life' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:Difers/Difer.git
+else
+  msg='来自github action的自动部署'
+  githubUrl=https://github.com/Difers/Difer.git
+  git config --global user.name "Difers"
+  git config --global user.email "c7070655110@gmail.com"
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:master # 推送到github
+
+# deploy to coding
+echo 'difer.life' > CNAME  # 自定义域名
+if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+  codingUrl=git@e.coding.net:difer/Difer.git
+else
+  codingUrl=https://e.coding.net/difer/Difer.git
+fi
+git add -A
+git commit -m "${msg}"
+git push -f $codingUrl master # 推送到coding
+
