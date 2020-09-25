e8bf0a962502922e2fc764906acee52d4c691c7a
root <c7070655110@gmail.com>
2020-09-25 08:51:14 +0800

    add push.sh

---
+++ static/baiduPush.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+path=$(dirname "$PWD")"/.notion"
+echo $path;
+ 
+files=$(ls $path)
+head=$1
+for filename in $files
+do 
+  filename=${filename%.*}
+  filename=$(echo $filename | tr -d "-")
+  echo $filename
+  echo "https://${head}/${filename}" > urls.txt
+done
+
+set -e
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.difer.life&token=NlmhlJXmwqWlcQcQ"
+rm -rf urls.txt
